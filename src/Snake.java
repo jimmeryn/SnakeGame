2b5b202e762ed0f36136f0d61463fe106f1a72b9
jimmeryn <jimmeryn@gmail.com>
2020-06-10 20:43:22 +0200

    Direction changing and frame collision

--- src/Snake.java
+++ src/Snake.java
@@ -5,7 +5,7 @@ public class Snake {
 	public SnakePart bodyPart;
 	public ArrayList<SnakePart> body = new ArrayList<SnakePart>();
 	public int x, y, size;
-	private boolean right = false, left = false, up = true, down = false; 
+	private boolean right = false, left = false, up = false, down = true; 
 	
 	public Snake(int x, int y) {
 		this.x = y;
@@ -35,5 +35,40 @@ public class Snake {
 		if(this.body.size() > 1) {
 			this.body.remove(0);
 		}
+	}
+	
+	public void Turn(String direction) {		
+		if(direction == "Up" && !this.down) {
+			this.right = false;
+			this.left = false;
+			this.up = true;
+		}
+		if(direction == "Down" && !this.up) {
+			this.right = false;
+			this.left = false;
+			this.down = true;
+		}
+		if(direction == "Left" && !this.right) {
+			this.up = false;
+			this.down = false;
+			this.left = true;
+		}
+		if(direction == "Right" && !this.left) {	
+			this.up = false;
+			this.down = false;
+			this.right = true;
+		}
+	}
+	
+	private boolean FrameCollision() {
+		if(x <= 0 || x >= 39 || y <= 0 || y >= 39) {
+			return true;
+		} else return false;		
+	}
+	
+	public boolean Collision() {
+		if(FrameCollision()) {
+			return true;
+		} else return false;
 	}
 }
