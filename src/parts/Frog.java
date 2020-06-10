f8eb3d64fd0ac41dc9e3c02c43f06e21aab96e91
jimmeryn <jimmeryn@gmail.com>
2020-06-10 23:01:48 +0200

    Frog movement basic directions

--- src/parts/Frog.java
+++ src/parts/Frog.java
@@ -2,11 +2,12 @@ package parts;
 import java.awt.Color;
 
 public class Frog extends Part{
+	public boolean up, down, left, right;
 	public Frog(int x, int y, int tileSize, Color color) {
 		super(x, y, tileSize, color);
 	}
 	
 	public Frog(int x, int y, int tileSize) {
-		super(x, y, tileSize, new Color(153-102- 0));
+		super(x, y, tileSize, Color.blue);
 	}
 }
