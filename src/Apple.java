8ae229072ea5c95e48fd5f362905fa8ef8657270
jimmeryn <jimmeryn@gmail.com>
2020-06-10 20:42:18 +0200

    Generating apples

--- src/Apple.java
+++ src/Apple.java
@@ -1,4 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Apple {
+	private int x, y, width, height;
+	public Apple(int x, int y, int tileSize) {
+		this.x = x;
+		this.y = y;
+		this.width = tileSize;
+		this.height = tileSize;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.red);
+		g.fillRect(this.x*width, this.y*height, this.width, this.height);
+	}
 
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 }
---
+++ src/AppleGenerator.java
@@ -0,0 +1,46 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class AppleGenerator {
+	public Apple apple;
+	public ArrayList<Apple> apples;
+	private Thread thread;
+	private Random r;
+	
+	public AppleGenerator() {
+		apples = new ArrayList<Apple>();
+		this.start();
+	}
+	
+	public void start() {
+		thread = new Thread("AppleGenerator");
+		thread.start();
+	}
+	
+	public void Generate() {
+		if(this.apples.size() == 0) {
+			r = new Random();
+			int x = r.nextInt(39);
+			int y = r.nextInt(39);
+			apple = new Apple(x, y, 10);
+			apples.add(apple);
+		}
+	}
+	
+	public void tick(Snake s) {
+		for(int i = 0; i < apples.size(); i++) {
+			if(s.x == apples.get(i).getX() && s.y == apples.get(i).getY()) {
+				s.AddBodyPart();
+				apples.remove(i);
+				i--;
+			}
+		}
+	}
+	
+	public void Paint(Graphics g) {
+		for(int i = 0; i < apples.size(); i++) {
+			apples.get(i).draw(g);
+		}
+	}
+}
